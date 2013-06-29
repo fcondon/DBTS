8c0904255a848fad907faaf7ab5b52a1b26c46fe
Fiona Condon <frcondon@gmail.com>
2013-06-29 18:12:52 -0400

    Refactoring common db connection code & commenting db.js functions

--- db.js
+++ db.js
@@ -24,21 +24,22 @@ var MongoClient = require('mongodb').MongoClient
     , format = require('util').format;
 var db_path = 'mongodb://127.0.0.1:27017/streakdb';
 
+// params: string
 function log(path) {
     var access_record = {'request' : path};
     insert('access_logs', access_record);
 }
 
+// params: int, function
 function addStreak(user_id, callback) {
     var new_streak = new streak(user_id); // TODO: wait for user input to create
     console.log("new streak: %o", new_streak);
     insert('streak_counts', new_streak, callback);
 }
 
+// params: int, function
 function findStreak(user_id, callback) {
-    MongoClient.connect(db_path, function(err, db) {
-        if (err) throw err;
-
+    dbConnect(function(db) {
         var collection = db.collection('streak_counts');
         var cursor = collection.find({ "user_id" : user_id }).nextObject(function(err, doc) {
             if (err) throw err;
@@ -49,13 +50,36 @@ function findStreak(user_id, callback) {
     });
 }
 
+// params: streak
 function maybeResetStreak(streak) {
-    last_day = time_util.getLastStreakDate(streak.date, streak.streak_count);
-    time_util.getYesterday();
-    console.log("loaded %s, last good streak day = %o", streak.user_id, last_day);
-    // TODO: do something real
+    if (streak.date) {
+        last_day = time_util.getLastStreakDate(streak.date, streak.streak_count);
+        yesterday = time_util.getYesterday(new Date());
+        console.log("loaded %s, last good streak day = ", streak.user_id, last_day);
+        if (time_util.compareDates(yesterday, last_day) > 0) {
+            resetStreak(streak);
+        }
+    }
 }
 
+// params: streak
+function resetStreak(streak) {
+    streak.date = null;
+    streak.streak_count = 0;
+    updateStreak(streak);
+}
+
+// params: streak
+function updateStreak(streak) {
+    dbConnect(function(db) {
+         db.collection('streak_counts').insert(insertion_data, function(err, docs) {
+            if (err) throw err;
+            db.close();
+         });
+    });
+}
+
+// params: int, function
 function findOrCreateStreak(user_id, callback) {
     findStreak(user_id, function(doc) {
         if (doc) {
@@ -68,19 +92,26 @@ function findOrCreateStreak(user_id, callback) {
     });
 }
 
+// params: string, object, function 
 function insert(collection_name, insertion_data, callback) {
-    MongoClient.connect(db_path, function(err, db) {
-        if (err) throw err;
-
-         var collection = db.collection(collection_name);
-         collection.insert(insertion_data, function(err, docs) {
+    dbConnect(function(db) {
+        var collection = db.collection(collection_name);
+        collection.insert(insertion_data, function(err, docs) {
             if (err) throw err;
             if (callback) {
                 callback(docs[0]);
             }
-            db.close();
-         });
+        });
+        db.close();
     });
+}
+
+// params: function
+function dbConnect(callback) {
+    MongoClient.connect(db_path, function(err, db) {
+        if (err) throw err;
+        callback(db);
+     });
 }
 
 exports.addStreak = addStreak;

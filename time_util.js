d3e0092dbc05c65db464947a46aa823ab9f09caf
Fiona Condon <frcondon@gmail.com>
2013-06-29 17:34:19 -0400

    Updating comments

--- time_util.js
+++ time_util.js
@@ -11,17 +11,22 @@ function dateInfo(date) {
     this.year = date.getFullYear();
 }
 
+// params: Date
+// return: dateInfo
 function getStorableDate(date) {
     return new dateInfo(date);
 }
 
+// params: Date, int
+// return: Date
 function getLastStreakDate(start_date, streak_count) {
     start_date = new Date(start_date.year, start_date.month, start_date.day);
     last_date = new Date(start_date.getTime() + (streak_count * MILLISECONDS_PER_DAY));
     return last_date;
 }
 
-// param Date
+// params: Date
+// return: int
 function compareDates(date1, date2) {
     var year_compare = compareNumbers(date1.getFullYear(), date2.getFullYear());
     if (year_compare === 0) {
@@ -35,12 +40,15 @@ function compareDates(date1, date2) {
     return year_compare;
 }
 
-// returns date
+// params: Date
+// return: Date
 function getYesterday(today) {
     yesterday = today.setDate(today.getDate() - 1); // who knew?
     return new Date(yesterday);
 }
 
+// params: int, int
+// return: int
 function compareNumbers(number1, number2) {
     return number1 - number2;
 }

4985f41ffe8a4948e400b825062f1e2c6e357655
Fiona Condon <frcondon@gmail.com>
2013-07-07 02:23:06 -0400

    Handlebar tpl actually compiles

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,5 @@
-(function() {
-    console.log("home invoked");
-})();
+$(document).ready(function() {
+    var header_source = $('#header_template').html();
+    var template      = Handlebars.compile(header_source);
+    $('#header').html(template({}));
+});

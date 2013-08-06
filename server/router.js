26964900a010910292075ff3ab62398334934ccb
Fiona Condon <frcondon@gmail.com>
2013-08-06 15:42:44 -0700

    Making the backend endpoints use-able

--- server/router.js
+++ server/router.js
@@ -3,22 +3,31 @@ var streak_controller = require("./streak_controller");
 var root = "/";
 var icon = "/favicon.ico";
 
-function route(pathname) {
-    switch (pathname) {
-        default:
-            var user_id = parseInt(pathname.substring(1));
-            if (user_id) {
-                streak_controller.getStreak(user_id, function(record) {
-                    if (record) {
-                        streak_controller.maybeResetStreak(streak);
-                        logStreak(record);
-                    }
-                    streak_controller.incrementOrCreateStreak(user_id);
-                });
-            } else {
-                console.log("path = " + pathname);
-                console.log("Should generate user id");
-            }
+function route(path) {
+    var path_elements = pathname.split('/');
+    if (path_elements.length > 1) {
+        var action = path_elements.shift();
+        var user_id = parseInt(pathname.shift());
+        console.log("Action = " + action + ", id = " + user_id);
+        switch (action) {
+            case "get":
+                if (user_id) {
+                    streak_controller.getStreak(user_id, function(record) {
+                        if (record) {
+                            // TODO: dump streak data for consumption
+                            logStreak(record);
+                        }
+                    });
+                } else {
+                    console.log("Streak for id " + user_id + " not found");
+                }
+            case "update":
+                streak_controller.incrementOrCreateStreak(user_id);
+            case "new":
+                streak_controller.createStreak();
+            default:
+                console.log("Unrecognized command: " + action);
+        }
     }
 }
 

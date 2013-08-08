adb52d987d09d966ba8a55e5496c38e9e0617fc6
fcondon <frcondon@gmail.com>
2013-08-08 00:54:25 -0700

    Actually pull data from server into client and render in a template

--- public/js/home.js
+++ public/js/home.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
-    var header_source = $.ajax({
-        url: '../templates/header.handlebars',
-        success: function(header_source) {
-            var template = Handlebars.compile(header_source);
-            $('#header').html(template({}));
+
+    var id = location.pathname.substr(1);
+    if (parseInt(id)) {
+        var get_url = '/get/' + id;
+    } else {
+        // TODO: no ID
+    }
+
+    // pull data about current streak
+    $.ajax({
+        url: get_url,
+        success: function(data) {
+            var streak = JSON.parse(data);
+
+            // get template
+            $.ajax({
+                url: '../templates/header.handlebars',
+                success: function(header_source) {
+                    var template = Handlebars.compile(header_source);
+                    $('#header').html(template({ 'streak_count' : streak.streak_count }));
+                }
+            });
         }
     });
+
 });

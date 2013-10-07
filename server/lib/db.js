7c6ac1b8adae3102bbb3f2286563b86a6fdbd007
fcondon <frcondon@gmail.com>
2013-10-06 21:55:49 -0500

    Adding notion of retries to db in attempt to thwart app failures

--- server/lib/db.js
+++ server/lib/db.js
@@ -3,17 +3,36 @@ var MongoClient = require('mongodb').MongoClient
 var db_path = 'mongodb://127.0.0.1:27017/streakdb';
 
 var streak_collection = 'streak_counts';
+var retries = 3;
 
 // params: function
-function connect(callback) {
-    MongoClient.connect(db_path, function(err, db) {
-        if (err) throw err;
-        callback(db);
-     });
+function connect(callback, tries) {
+    if (tries < retries) {
+        try {
+            MongoClient.connect(db_path, function(err, db) {
+                // don't start no shit, won't be no shit
+                if (err) throw err;
+                callback(db);
+            });
+        } catch (err) {
+            handleDBError(err);
+        }
+    } else {
+        throw "Could not connect to DB";
+    }
+}
+
+function handleDBError(err, callback, tries) {
+    console.log("DB connection error: %s (%d tries)", err, tries);
+
+    // retry
+    tries++;
+    connect(callback, handleDBError, tries);
 }
 
 // params: int, function
 function findStreak(user_id, callback) {
+    console.log('finding ' + user_id);
     connect(function(db_connection) {
         var collection = db_connection.collection(streak_collection);
         var cursor = collection.find({ "user_id" : user_id }).nextObject(function(err, streak) {
@@ -22,7 +41,7 @@ function findStreak(user_id, callback) {
                 callback(streak);
             }
         });
-    });
+    }, 1);
 }
 
 // params: streak
@@ -33,7 +52,7 @@ function saveStreak(streak) {
             if (err) throw err;
         });
         db_connection.close();
-    });
+    }, 1);
 }
 
 // params: string, object, function
@@ -47,7 +66,7 @@ function insertStreak(insertion_data, callback) {
             }
         });
         db_connection.close();
-    });
+    }, 1);
 }
 
 exports.findStreak = findStreak;
